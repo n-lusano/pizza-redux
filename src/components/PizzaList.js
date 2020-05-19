0c3aa1a99c1fff08aaaad0e7af8aa939639537fb
N-LUSANO <N.LUSANO@GMAIL.COM>
2020-05-19 13:23:51 +0200

    Select pizzas and display in components

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -2,16 +2,32 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function selectUser(reduxState) {
-  console.log("State in selector", reduxState);
+  //   console.log("State in selector", reduxState);
   return reduxState.user;
+}
+
+function selectPizzas(reduxState) {
+  return reduxState.pizzas;
 }
 
 export default function PizzaList() {
   const user = useSelector(selectUser);
-  console.log("User in component", user);
+  const pizzas = useSelector(selectPizzas);
+  //   console.log("User in component", user);
+  console.log("Pizzas in component", pizzas);
   return (
     <div>
       <h1>Hello {user.name}</h1>
+      <ul>
+        {pizzas.map((pizza) => {
+          return (
+            <li key={pizza.id}>
+              <h3>{pizza.name}</h3>
+              <p>{pizza.description}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
